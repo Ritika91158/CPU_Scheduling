a5fa81cb1bf858a010e3b4041bf4baf22445337e
Ritika91158 <148696166+Ritika91158@users.noreply.github.com>
2025-03-24 13:58:34 +0530

    Update sjf.cpp

--- sjf.cpp
+++ sjf.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void sjfScheduling(vector<int>& processes, int n, vector<int>& burst_time) {
-    vector<int> waiting_time(n), turnaround_time(n);
+void sjfScheduling(int processes[], int n, int burst_time[]) {
+    int waiting_time[n], turnaround_time[n];
     int temp_burst, temp_pid;
 
     for (int i = 0; i < n - 1; i++) {
@@ -28,6 +27,7 @@ void sjfScheduling(vector<int>& processes, int n, vector<int>& burst_time) {
         turnaround_time[i] = burst_time[i] + waiting_time[i];
     }
 
+    cout << "SJF Scheduling:\n";
     cout << "Process\tBurst Time\tWaiting Time\tTurnaround Time\n";
     for (int i = 0; i < n; i++) {
         cout << processes[i] << "\t" << burst_time[i] << "\t\t" << waiting_time[i] << "\t\t" << turnaround_time[i] << endl;
@@ -43,9 +43,9 @@ void sjfScheduling(vector<int>& processes, int n, vector<int>& burst_time) {
 }
 
 int main() {
-    vector<int> processes = {1, 2, 3};
-    vector<int> burst_time = {5, 2, 8};
-    int n = processes.size();
+    int processes[] = {1, 2, 3};
+    int burst_time[] = {5, 2, 8};
+    int n = sizeof(processes) / sizeof(processes[0]);
     
     sjfScheduling(processes, n, burst_time);
     
